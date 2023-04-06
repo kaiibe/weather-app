ef23e8d113308e42cef36fa09d301154bd70cbde
kaiibe <kaiibe@mail.ru>
2023-04-06 22:41:17 +0200

    fixed stracture

--- lib/UI/home/home.dart
+++ lib/UI/home/home.dart
@@ -3,8 +3,8 @@
 import 'package:flutter/material.dart';
 
 import '/models/constants.dart';
-import 'widgets/app_bar.dart';
 import './widgets/my_location_weather.dart';
+import './widgets/cities_weather.dart';
 
 class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
@@ -18,15 +18,65 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     Constants myConstants = Constants();
+
     final editOptions = ["Edit", "C", "F"];
+    final myCities = ["Berlin", "Dushanbe"];
+
+    final appBar = AppBar(
+      backgroundColor: myConstants.pageColor,
+      elevation: 0.0,
+      centerTitle: false,
+      titleSpacing: 0,
+      title: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        width: size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Weather",
+              style: TextStyle(
+                fontFamily: 'RussoOne',
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        PopupMenuButton(
+          onSelected: (selectedValue) {
+            if (selectedValue == "Add") {
+              myCities.add("Berlin");
+            }
+          },
+          icon: Icon(Icons.menu),
+          itemBuilder: (_) => [
+            PopupMenuItem(
+              value: "Add",
+              child: Text("Add"),
+            ),
+            PopupMenuItem(
+              value: "C",
+              child: Text("C"),
+            ),
+            PopupMenuItem(
+              value: "F",
+              child: Text("F"),
+            ),
+          ],
+        )
+      ],
+    );
 
     return Scaffold(
         backgroundColor: myConstants.pageColor,
-        appBar: myAppBar(context),
+        appBar: appBar,
         body: ListView(
           padding: EdgeInsets.all(10),
           children: <Widget>[
             MyLocationWeather(),
+            CitiesWeather("Some"),
           ],
         ));
   }
